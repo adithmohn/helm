5ab563b65cca8cad950282d526d3e5a15042ff0d
Adith Mohan <101926279+adithmohn@users.noreply.github.com>
2024-02-26 12:43:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! worked ok, push worked today  worked  ok bnbmn');
+  res.send('Hello, microk8s');
 });
 
 app.listen(port, () => {
